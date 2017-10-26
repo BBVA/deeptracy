992f6133b49b71765e084f81f57f6d7f3471f3a7
Alejandro Gonzalez Rodrigo <alejandrogr@gmail.com>
2017-10-26 16:26:51 +0200

    add base task class

---
+++ deeptracy/tasks/base_task.py
@@ -0,0 +1,19 @@
+"""This module contains base class for all celery task in deeptracy and other common classes used in all tasks"""
+import logging
+from celery import Task
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+class DeeptracyTask(Task):
+    """Default class for all task in deeptracy. It has error handling for logging all celery failures in tasks"""
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        logger.info('-----ON FAILURE-----')
+        logger.exception('celery task failure', exc_info=exc)
+        super().on_failure(exc, task_id, args, kwargs, einfo)
+
+
+class TaskException(BaseException):
+    """Exception for use in controlled errors inside tasks"""
+    pass
