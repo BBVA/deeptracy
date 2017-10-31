c662f528e8afb0b575686d1d847a1ad0969cdf11
Alejandro Gonzalez Rodrigo <alejandrogr@gmail.com>
2017-10-31 15:09:28 +0100

    add missing taks to celery loaded tasks

--- deeptracy/celery.py
+++ deeptracy/celery.py
@@ -16,9 +16,10 @@
 Module for deeptracy
 """
 from celery import Celery
-from deeptracy.config import BROKER_URI
+
 from deeptracy_core.dal.database import db
-from deeptracy.plugin_store import plugin_store
+from .config import BROKER_URI
+from .plugin_store import plugin_store
 
 db.init_engine()  # Init database engine
 plugin_store.load_plugins()  # Load analyzer plugins
@@ -31,5 +32,6 @@ celery = Celery('deeptracy',
                 include=[
                     'deeptracy.tasks.start_scan',
                     'deeptracy.tasks.run_analyzer',
-                    'deeptracy.tasks.merge_results'
+                    'deeptracy.tasks.merge_results',
+                    'deeptracy.tasks.notify_results'
                 ])
